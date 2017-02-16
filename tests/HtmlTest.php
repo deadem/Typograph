c2e4056d2b6f2be3849664e8841b99a2dbd1e065
deadem <deadem@gmail.com>
2017-02-16 16:20:59 +0300

    Update HtmlTest.php

--- tests/HtmlTest.php
+++ tests/HtmlTest.php
@@ -4,7 +4,7 @@ namespace Test;
 
 use DJEM\Typograph;
 
-class HtmlTest extends \PHPUNIT_Framework_Testcase
+class HtmlTest extends \PHPUnit_Framework_TestCase
 {
     public function testHtml()
     {
